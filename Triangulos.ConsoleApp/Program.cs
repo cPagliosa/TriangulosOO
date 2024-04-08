0fbb5d794f8f9c5b08e46692252dfec9e6e65569
Caio Pagliosa <caio.pinipagli@gmail.com>
2024-04-08 17:20:08 -0300

    Add project files.

---
+++ Triangulos.ConsoleApp/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Triangulos.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo triangulo = new Triangulo();
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("****************************************\n" +
+                              "**   Confira o tipo do seu Triangulo  **\n" +
+                              "****************************************");
+
+                //entrada
+                Entrada(triangulo);
+
+                //validar triagulo e da a saida
+                Saida(triangulo);
+
+                Console.WriteLine("\nVocê quer fazer?\n[1] Sim   [2] Não");
+                Console.Write("qual a sua escolha: ");
+                int h = Convert.ToInt32(Console.ReadLine());
+                if (h == 2) break;
+            }
+
+
+            
+        }
+
+        private static void Saida(Triangulo triangulo)
+        {
+            if (triangulo.ValidarTriangulo(triangulo.Altura, triangulo.Largura, triangulo.Comprimento))
+            {
+                Console.WriteLine($"\nO seu Triangulo é do tipo {triangulo.ReconhecerTriangulo(triangulo.Altura, triangulo.Largura, triangulo.Comprimento)}.");
+            }
+            else
+            {
+                Console.WriteLine("Triangulo Invalido!!");
+            }
+        }
+
+        private static void Entrada(Triangulo triangulo)
+        {
+            Console.Write("Informe o comprimento do triangulo: ");
+            triangulo.Comprimento = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Informe o altura do triangulo: ");
+            triangulo.Altura = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Informe o largura do triangulo: ");
+            triangulo.Largura = Convert.ToDecimal(Console.ReadLine());
+        }
+    }
+}
---
+++ Triangulos.ConsoleApp/Triangulo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulos.ConsoleApp
+{
+    internal class Triangulo
+    {
+        private decimal largura;
+        private decimal comprimento;
+        private decimal altura;
+
+        //gerar gets e sets
+        public decimal Largura { get => largura; set => largura = value; }
+        public decimal Comprimento { get => comprimento; set => comprimento = value; }
+        public decimal Altura { get => altura; set => altura = value; }
+
+        //contrutor
+        public Triangulo()
+        {
+        }
+
+        public Boolean ValidarTriangulo(decimal x, decimal y, decimal z)
+        {
+            if ((x + y) >= z && (x + z) >= y && (y + z) >= x) return true;
+            else return false;
+
+        }
+        
+        public String ReconhecerTriangulo(decimal x, decimal y, decimal z)
+        {
+            if (this.Equilatero(x, y, z)) return "Triangulo Equilatero";
+            else if (this.Isosceles(x, y, z)) return "Tringulo Isosceles";
+            else if (this.Escaleno(x, y, z)) return "Tringulo Escaleno";
+            else return "Erro na operação!!";
+        }
+
+        private Boolean Equilatero(decimal x, decimal y,decimal z)
+        {
+            if(x == y && y == z) return true;
+            else return false;
+        }
+        private Boolean Isosceles(decimal x, decimal y, decimal z)
+        {
+            if(x == y && x != z) return true;
+            else if(y == z && y != x) return true;
+            else if(z == x && y != z) return true;
+            else return false;
+        }
+        private Boolean Escaleno(decimal x, decimal y, decimal z)
+        {
+            if(x != y && x != z && y != z) return true;
+            else return false;
+        }
+    }
+}
